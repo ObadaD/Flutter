9a7001eadcdc9e21aac9169cd1cb3404bfc4ed59
Obada <obadaageeb22@gmail.com>
2019-11-03 11:13:15 +0200

    first commit

---
+++ android/app/src/main/java/com/obada/application/textrecognation/MainActivity.java
@@ -0,0 +1,13 @@
+package com.obada.application.textrecognation;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
